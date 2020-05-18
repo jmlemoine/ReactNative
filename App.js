4bb548ada0e74f2402f462f330ee008670ce081b
jmlemoine <jeanmelvinlp27@gmail.com>
2020-05-18 07:48:16 -0400

    Perfil

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image, Button, Alert } from "react-native";
 
 export default function App() {
+  klk = () => {
+    Alert.alert("Hola Jean Melvin");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <Image
+            source={require("./assets/Perfil.jpeg")}
+            style={styles.logo}
+          ></Image>
+        </View>
+
+        <View style={styles.headerRight}>
+          <Button title="Login" onPress={klk} />
+        </View>
+      </View>
+
+      <View style={(styles.body, styles.alla)}>
+        <Text>Hola Jean Melvin!</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +31,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    //backgroundColor: "blue",
+    alignItems: "center",
+    //justifyContent: "center",*/
+  },
+  alla: {
+    fontWeight: "bold",
+  },
+  header: {
+    flex: 0.3,
+    flexDirection: "row",
+    marginTop: 40,
+  },
+  headerLeft: {
+    flex: 1,
+    //backgroundColor: "red",
+  },
+  headerRight: {
+    flex: 1,
+    //backgroundColor: "green",
+  },
+  body: {
+    flex: 1,
+    alignItems: "center",
+    //backgroundColor: "yellow",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    resizeMode: "contain",
   },
 });
